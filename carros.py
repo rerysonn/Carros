84c6cc5c70ed20bb8402f81e70f05df9b6903e81
Reryson Farinha <rerysonfarinha@outlook.com>
2024-03-07 16:31:18 -0300

    Implementacao de metodos

--- carros.py
+++ carros.py
@@ -1,5 +1,24 @@
 class Carros:
-    def __init__(self, modelo, pintura, disponivel):
+    carros = []
+
+    def __init__(self, modelo, pintura):
         self.modelo = modelo
         self.pintura = pintura
         self.disponivel = False
+
+        Carros.carros.append(self)
+
+    def __str__(self):
+        return f"{self.modelo} | {self.pintura}"
+    
+    @classmethod
+    def listar_carros(cls):
+        for carro in cls.carros:
+            print(f"{carro.modelo} | {carro.pintura}")
+
+
+carro1 = Carros("palio", "verde")
+carro2 = Carros("fusca", "preto")
+carro3 = Carros("nivus", "azul")
+
+Carros.listar_carros()
