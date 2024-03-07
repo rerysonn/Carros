5bd929904cd8a9fece823a6a457836a038359138
Reryson Farinha <rerysonfarinha@outlook.com>
2024-03-07 18:05:03 -0300

    Implementacao do atributo Avaliacao

--- app.py
+++ app.py
@@ -6,6 +6,14 @@ carro2 = Carros("fusca", "preto")
 carro3 = Carros("nivus", "azul")
 carro3.alterar_estado()
 
+carro1.receber_avaliacao("Mario", 3.5)
+carro1.receber_avaliacao("Sergio", 4.8)
+
+carro2.receber_avaliacao("Nicolas", 5)
+carro2.receber_avaliacao("Pedro", 4)
+
+
+
 
 def main():
     Carros.listar_carros()
--- carros.py
+++ carros.py
@@ -1,21 +1,24 @@
+from avaliacao import Avaliacao
+
 class Carros:
     carros = []
 
     def __init__(self, modelo, pintura):
-        self.modelo = modelo.title()
-        self.pintura = pintura.title()
+        self._modelo = modelo.title()
+        self._pintura = pintura.title()
+        self._avaliacao = []
         self._disponivel = False
 
         Carros.carros.append(self)
 
     def __str__(self):
-        return f"{self.modelo} | {self.pintura}"
+        return f"{self._modelo} | {self._pintura}"
     
     @classmethod
     def listar_carros(cls):
-        print(f"{"Modelo do carro".ljust(20)} | {"Cor do carro".ljust(20)} | {"Status do carro"}")
+        print(f"{"Modelo do carro".ljust(20)} | {"Cor do carro".ljust(20)} | {"Avaliação".ljust(20)} | {"Status do carro"}")
         for carro in cls.carros:
-            print(f"{carro.modelo.ljust(20)} | {carro.pintura.ljust(20)} | {carro.carro_disponivel}")
+            print(f"{carro._modelo.ljust(20)} | {carro._pintura.ljust(20)} | {str(carro.media_avaliacao).ljust(20)} |{carro.carro_disponivel}")
 
     @property
     def carro_disponivel(self):
@@ -23,6 +26,20 @@ class Carros:
     
     def alterar_estado(self):
         self._disponivel = not self._disponivel
+
+    def receber_avaliacao(self, cliente, nota):
+        if 0 < nota <= 5:
+            avaliacao = Avaliacao(cliente, nota)
+            self._avaliacao.append(avaliacao)
+
+    @property
+    def media_avaliacao(self):
+        if not self._avaliacao:
+            return 'Sem Avaliação'
+        soma_notas = sum(avaliacao._nota for avaliacao in self._avaliacao)
+        quantidade_notas = len(self._avaliacao)
+        media = round(soma_notas/quantidade_notas, 1)
+        return media
 
 
 
